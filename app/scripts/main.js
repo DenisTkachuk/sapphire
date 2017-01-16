2cffb21ed7ea95fe1897f0dda496b08005a171d3
DenisTkachuk <denis.tkachuk@gmail.com>
2017-01-17 01:52:32 +0200

    carousel
    add carousel
    add manific popup
    set js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,74 @@
+$(document).ready(function() {
 
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+
+  $(".slider").slick({
+    infinite: true,
+    dots: true,
+    lazyLoad: 'progressive',
+    autoplaySpeed: 5000,
+    autoplay: true,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    speed: 500
+  });
+
+  var $carousel = $('.carousel');
+
+  $carousel
+    .slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      focusOnSelect: true,
+      lazyload: 'progressive',
+      responsive: [
+        {
+          breakpoint: 1930,
+          settings: {
+            slidesToShow: 9
+          }
+        },
+        {
+          breakpoint: 1440,
+          settings: {
+            slidesToShow: 6
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    })
+    .magnificPopup({
+      type: 'image',
+      delegate: 'a:not(.slick-cloned)',
+      gallery: {
+        enabled: true
+      },
+      callbacks: {
+        open: function() {
+          var current = $carousel.slick('slickCurrentSlide');
+          $carousel.magnificPopup('goTo', current);
+        },
+        beforeClose: function() {
+          $carousel.slick('slickGoTo', parseInt(this.index));
+        }
+      }
+    });
+});
 
